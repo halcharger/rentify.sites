3b8345716607d5c33c448c1704c7ecf6686711a9
halcharger <allen.firth@gmail.com>
2014-12-01 14:09:54 +0200

    initial commit

---
+++ Rentify.Sites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rentify.Sites.Startup))]
+namespace Rentify.Sites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
